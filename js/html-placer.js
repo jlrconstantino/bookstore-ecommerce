034daf0adec4be93118daea9421c7c9a1d382a95
jlrconst <jlconsttantino@live.com>
2022-04-21 15:39:48 -0300

    fixed javascript to place navigation bar in HTML files

--- js/html-placer.js
+++
@@ -1,9 +0,0 @@
-
-fetch(document.currentScript.getAttribute("html_path"))
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#".concat(document.currentScript.getAttribute("id")));
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
---
+++ js/top-nav-bar-placer.js
@@ -0,0 +1,15 @@
+// Substitui a chamada do script por um documento HTML de barra de navegação superior em tempo de execução
+fetch("../html/top-nav-bar.html")
+.then(res => res.text())
+.then(text => {
+
+    // Seleciona o script de substituição
+    let old_element = document.querySelector("script#top-nav-bar-placer");
+
+    // Cria o novo elemento com base no HTML da barra de navegação
+    let new_element = document.createElement("div");
+    new_element.innerHTML = text;
+
+    // Substitui no HTML em tempo-real
+    old_element.parentNode.replaceChild(new_element, old_element);
+})
