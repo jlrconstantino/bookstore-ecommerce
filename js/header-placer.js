b4ecc87e06578e6b7ee26e8557d9091b61092dfb
jlrconst <jlconsttantino@live.com>
2022-04-21 17:56:02 -0300

    replaced top-nav-bar with header

--- js/header-placer.js
+++ js/header-placer.js
@@ -1,14 +1,14 @@
 // Substitui a chamada do script por um documento HTML de barra de navegação superior em tempo de execução
-fetch("../html/top-nav-bar.html")
+fetch("../html/header.html")
 .then(res => res.text())
 .then(text => {
 
     // Seleciona o script de substituição
-    let old_element = document.querySelector("script#top-nav-bar-placer");
+    let old_element = document.querySelector("script#header-placer");
 
     // Cria o novo elemento com base no HTML da barra de navegação
     let new_element = document.createElement("nav");
-    new_element.setAttribute("id", "top-nav-bar")
+    new_element.setAttribute("id", "header")
     new_element.innerHTML = text;
 
     // Substitui no HTML em tempo-real
