5f299532b64c4bd9801e66ac4a07cd84e25a2282
jlrconst <jlconsttantino@live.com>
2022-04-23 03:36:13 -0300

    added new javascript features

--- js/book-displayer-placer.js
+++ js/book-displayer-placer.js
@@ -3,14 +3,22 @@ fetch("../elements/book-displayer.html")
 .then(res => res.text())
 .then(text => {
 
-    // Seleciona o script de substituição
-    let old_element = document.querySelector("script#book-displayer-placer");
+    // Seleciona os scripts de substituição
+    let old_elements = document.querySelectorAll("script#book-displayer-placer");
 
-    // Cria o novo elemento com base no HTML do mostrador
-    let new_element = document.createElement("div");
-    new_element.setAttribute("class", "book-displayer-container");
-    new_element.innerHTML = text;
+    // Cria o novo elemento com base no HTML do mostrador para cada script
+    old_elements.forEach(old_element => {
+        let new_element = document.createElement("div");
+        new_element.setAttribute("class", "book-displayer-container");
+        new_element.innerHTML = text;
 
-    // Substitui no HTML em tempo-real
-    old_element.parentNode.replaceChild(new_element, old_element);
+        // Atribui as variáveis
+        new_element.getElementsByClassName("book-displayer-img")[0].setAttribute("src", old_element.getAttribute("img-src"));
+        new_element.getElementsByClassName("book-displayer-title")[0].innerHTML = old_element.getAttribute("title");
+        new_element.getElementsByClassName("book-displayer-price")[0].innerHTML = old_element.getAttribute("price");
+        new_element.getElementsByClassName("book-displayer-button")[0].onclick = function(){location.replace(old_element.getAttribute("target"))};
+
+        // Substitui no HTML em tempo-real
+        old_element.parentNode.replaceChild(new_element, old_element);
+    })
 })
